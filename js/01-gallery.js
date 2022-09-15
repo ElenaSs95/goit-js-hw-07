4cbd8f42f7879ba10b56364bb9cf00173bed233e
Елена <lenochkashevchenko95@gmail.com>
2022-09-15 13:54:19 +0300

    ДЗ-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.insertAdjacentHTML(
+    'afterbegin',
+    galleryItems
+        .map(
+            galleryItem =>
+                `<div class="gallery__item">
+        <a clss="gallery__link" href="${galleryItem.original}">
+        <img class="gallery__image"
+        src="${galleryItem.preview}" 
+        data-source="${galleryItem.original}"
+        alt= "${galleryItem.description}"></a></div>`,
+        )
+        .join(''),
+);
+
+gallery.addEventListener('click', selectedImage);
+
+function selectedImage(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    const imgLink = e.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${imgLink}">`);
+    instance.show();
+
+    document.addEventListener('keydown', e => {
+        if (e.key === 'Escape') instance.close();
+    });
+}
